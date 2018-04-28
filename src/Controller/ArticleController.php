106ecd8790864910764bb9269dfde1e8f81d7ec4
KillianRDJ <killian@ruedesjoueurs.com>
2018-04-29 00:28:17 +0200

    ajout article
    ajout article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Article;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 class ArticleController extends Controller
 {
@@ -12,8 +13,32 @@ class ArticleController extends Controller
      */
     public function index()
     {
+               
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function showArticle($id)
+    {
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article found for id '.$id
+            );
+        }
+
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        return $this->render('article/index.html.twig', ['article' => $article]);
+    }
+  
+
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -34,4 +34,28 @@ class Article
     {
         return $this->id;
     }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
 }
